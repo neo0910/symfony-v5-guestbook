2e05ff7d92108c752f72afbd1f8fae50549a13ad
neo0910 <denikkalenik@gmail.com>
2020-12-31 16:26:31 +0300

    the first 15 lessons added

---
+++ src/Controller/Admin/CommentCrudController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CommentCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Comment::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            AssociationField::new('conference'),
+            DateField::new('createdAt'),
+            EmailField::new('email'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('author'),
+            TextField::new('photoFilename'),
+            TextField::new('text'),
+            ImageField::new('photoFilename')->setBasePath('/uploads/photos')->setLabel('Photo')->hideOnForm(),
+        ];
+    }
+}
---
+++ src/Controller/Admin/ConferenceCrudController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class ConferenceCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Conference::class;
+    }
+}
